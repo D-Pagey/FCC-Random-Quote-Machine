7bc3bae666e5538654ec81083a9e2d422a47b749
D-Pagey <dan.page91@gmail.com>
2017-10-26 23:51:47 +0100

    Sorted click event listener.

--- scripts.js
+++ scripts.js
@@ -3,28 +3,19 @@ const artistEl = document.getElementById("artist");
 
 url = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";
 
+function quoter() {
 fetch(url)
   .then((resp) => resp.json())
   .then(function(data) {
     console.log(data);
-
-  function newQuote() {
-      quoteEl.innerHTML = data[0].content;
-      artistEl.innerHTML = data[0].title;
-    }
-
-newQuote();
-
-    document.getElementById("button").addEventListener("click", newQuote);
-
+    quoteEl.innerHTML = data[0].content;
+    artistEl.innerHTML = data[0].title;
 })
 
   .catch(function(error) {
     console.log("Something went wrong");
 })
 
-/*
-User Story: I can click a button to show me a new random quote.
+}
 
-User Story: I can press a button to tweet out a quote.
-*/
+document.getElementById("button").addEventListener("click", quoter);
