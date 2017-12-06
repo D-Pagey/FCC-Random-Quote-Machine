e5d92d763da12b5bf3e8f8570a418b5daf6c6990
Pagey <dan.page91@gmail.com>
2017-12-06 12:51:20 +0000

    Starting disabling button in between requests.

--- scripts.js
+++ scripts.js
@@ -1,17 +1,33 @@
 const quoteEl = document.getElementById("quote");
 const artistEl = document.getElementById("artist");
+const buttonEl = document.getElementById("button");
 const url = "https://talaikis.com/api/quotes/random/";
+
+function btnState() {
+  buttonEl.setAttribute("disabled", true);
+  buttonEl.removeAttribute("disabled");
+}
+
+btnState();
+
+function changeBtnState() {
+
+}
+
+// need to disable button until receive response 
 
 let responseData;
 console.log(responseData);
 
 function quoter() {
+
 fetch(url)
   .then((resp) => resp.json())
   .then(function(data) {
     responseData = data;
     quoteEl.innerHTML = data.quote;
     artistEl.innerHTML = data.author;
+
 })
 
   .catch(function(error) {
@@ -19,7 +35,7 @@ fetch(url)
 })
 }
 
-document.getElementById("button").addEventListener("click", quoter);
+buttonEl.addEventListener("click", quoter);
 
 const twitterUrl = "https://twitter.com/intent/tweet?text=";
 
